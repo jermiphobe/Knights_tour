2903b5ca209c47918dbdd32ef7261f9a54a11f5d
jermiphobe <65186585+jermiphobe@users.noreply.github.com>
2020-06-09 16:09:45 -0500

    Create Knights_graphics.java

---
+++ src/Knights_graphics.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Knights_graphics {
+
+}
+
+class Knights_canvas extends JPanel {
+	
+}
+
+class Knights_frame extends JFrame {
+	
+}
+
+class Knights_square {
+	
+}
