f470ad7c0beace36614fa49ca509e29c481e16e8
jermiphobe <65186585+jermiphobe@users.noreply.github.com>
2020-06-10 17:00:48 -0500

    Did the project

--- src/Knights_graphics.java
+++ src/Knights_graphics.java
@@ -1,18 +1,277 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Knights_graphics {
+	Knights_frame frame = new Knights_frame();
+	int window_size = 0;
+	int square_count = 0;
+	int square_side = 0;
 
-}
+	Knights_graphics(int size, int square_size, int squares) {
+		window_size = size;
+		square_count = squares;
+		square_side = square_size;
+		
+		//Initializes the frame 'settings'
+		frame.setTitle("Knight's Tour");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(size + 16, size + 39);
+		frame.setVisible(true);
+		frame.setLocation(400, 100);
+		
+		frame.create_squares(square_count, square_side);
+		
+	}
+	
+	public void fill_a_square(int id) {
+		frame.fill_a_square(id);
+	}
+	
+	public void unfill_a_square(int id) {
+		frame.unfill_a_square(id);
+	}
+	
+	public void solve_it() {
+		frame.solve_it();
+	}
+	
+	public void fail() {
+		frame.fail();
+	}
 
-class Knights_canvas extends JPanel {
 	
 }
 
 class Knights_frame extends JFrame {
+	Knights_canvas canvas = new Knights_canvas();
+	
+	Knights_frame() {
+		add(canvas);
+	}
+	
+	public void create_squares(int total_squares, int box_side) {
+		canvas.create_squares(total_squares, box_side);
+	}
+	
+	public void fill_a_square(int id) {
+		canvas.fill_a_square(id);
+		repaint();
+	}
+	
+	public void unfill_a_square(int id) {
+		canvas.unfill_a_square(id);
+		repaint();
+	}
+	
+	public void solve_it() {
+		canvas.solve_it();
+		repaint();
+	}
+	
+	public void fail() {
+		canvas.fail();
+		repaint();
+	}
+	
+}
+
+class Knights_canvas extends JPanel {
+	
+	ArrayList<Knights_square> squares = new ArrayList<>();
+	
+	
+	Knights_canvas() {
+	}
+	
+	public void create_squares(int total_squares, int box_side) {
+		int side_length = (int) Math.sqrt(total_squares);
+		int curr_square = 1;
+		int curr_x = 0;
+		int curr_y = 0;
+		
+		//Creates the square objects
+		for (int i = 0; i < side_length; i += 1) {
+			
+			for (int j = 0; j < side_length; j += 1) {
+				Knights_square square = new Knights_square(curr_x, curr_y, box_side, curr_square);
+				squares.add(square);
+				
+				//Increment curr_square 
+				curr_square += 1;
+				
+				//Increment curr_x
+				curr_x += box_side;
+			}
+			
+			//Add side length to the curr_y
+			curr_y += box_side;
+			
+			//Reset curr_x
+			curr_x = 0;
+		}
+		
+		repaint();
+	}
+	
+	public void fill_a_square(int id) {
+		for (Knights_square square: squares) {
+			if (square.get_id() == id) {
+				square.fill();
+				break;
+			}
+		}
+	}
+	
+	public void unfill_a_square(int id) {
+		for (Knights_square square: squares) {
+			if (square.get_id() == id) {
+				square.unfill();
+				break;
+			}
+		}
+	}
+	
+	public void solve_it() {
+		for (Knights_square square: squares) {
+			square.solve();
+		}
+	}
+	
+	public void fail() {
+		for (Knights_square square: squares) {
+			square.fill();
+		}
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		setBackground(Color.BLACK);
+		
+		for (Knights_square square: squares) {
+			
+			if (square.solved()) {
+				square.solved_square(g);
+				continue;
+			}
+			
+			if (square.filled()) {
+				square.fill_square(g);
+			} else {
+				square.draw_square(g);
+			}
+		}
+		
+		//repaint();
+	}
 	
 }
 
 class Knights_square {
+	int x_origin = 0;
+	int y_origin = 0;
+	int square_size = 0;
+	int id = 0;
+	String str_id = "";
 	
+	boolean filled = false;
+	boolean solved = false;
+	boolean open = true;
+	
+	Knights_square(int x, int y, int size, int square_id) {
+		x_origin = x;
+		y_origin = y;
+		square_size = size;
+		id = square_id;
+		str_id = String.valueOf(id);
+		
+	}
+	
+	//Draw the empty square
+	public void draw_square(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x_origin, y_origin, square_size, square_size);
+		
+		g.setColor(Color.black);
+		g.drawRect(x_origin, y_origin, square_size, square_size);
+		
+		Font font = new Font("Courier New", Font.BOLD, square_size / 4);
+		g.setFont(font);
+		g.setColor(Color.black);
+		g.drawString(str_id, x_origin + (square_size / 2), y_origin + (square_size / 2));
+	}
+	
+	//Draw a red square
+	public void fill_square(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(x_origin, y_origin, square_size, square_size);
+		
+		//Outline the square
+		g.setColor(Color.black);
+		g.drawRect(x_origin, y_origin, square_size, square_size);
+		
+		Font font = new Font("Courier New", Font.BOLD, square_size / 4);
+		g.setFont(font);
+		g.setColor(Color.black);
+		g.drawString(str_id, x_origin + (square_size / 2), y_origin + (square_size / 2));
+	}
+	
+	//Draw a green square
+	public void solved_square(Graphics g) {
+		g.setColor(Color.green);
+		g.fillRect(x_origin, y_origin, square_size, square_size);
+		
+		//Outline the square
+		g.setColor(Color.black);
+		g.drawRect(x_origin, y_origin, square_size, square_size);
+		
+		Font font = new Font("Courier New", Font.BOLD, square_size / 4);
+		g.setFont(font);
+		g.setColor(Color.black);
+		g.drawString(str_id, x_origin + (square_size / 2), y_origin + (square_size / 2));
+	}
+	
+	public void fill() {
+		filled = true;
+		open = false;
+	}
+	
+	public void unfill() {
+		filled = false;
+		open = true;
+	}
+	
+	public boolean filled() {
+		return filled;
+	}
+	
+	public int get_id() {
+		return id;
+	}
+	
+	public void solve() {
+		solved = true;
+	} 
+	
+	public boolean solved() {
+		return solved;
+	}
+	
+	public boolean is_open() {
+		return open;
+	}
+	
+	 
 }
---
+++ src/Knights_tour.java
@@ -0,0 +1,266 @@
+import java.awt.GraphicsEnvironment;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Knights_tour {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int total_boxes = 0;
+		int total_window_size = 1000;
+		
+		ArrayList<ArrayList<Knights_square>> squares = new ArrayList<>();
+		
+		//Input for how many boxes you want
+		while (true) {
+			System.out.print("How many squares do you want? > ");
+			String num_boxes = input.nextLine();
+			
+			
+			try {
+				total_boxes = Integer.parseInt(num_boxes);
+				break;
+				
+			} catch (NumberFormatException e) {
+				System.out.println();
+				continue;
+			}
+		}
+		
+		//Figure out side length and box size
+		int side_length = (int) Math.sqrt(total_boxes);
+		int square_size = (total_window_size - 15) / side_length;
+		
+		//Reset total_boxes and total_window_size to be accurate to what is shown on screen 
+		total_boxes = side_length * side_length;
+		total_window_size = side_length * square_size;
+		
+		System.out.println("Total boxes: " + total_boxes);
+		System.out.println("Side length: " + side_length);
+		System.out.println("Square size: " + square_size);
+		
+		Knights_graphics graphics = new Knights_graphics(total_window_size, square_size, total_boxes);
+		
+		//Create the array for logic side of program
+		int curr_square = 1;
+		int curr_x = 0;
+		int curr_y = 0;
+		
+		//Creates the square objects
+		for (int i = 0; i < side_length; i += 1) {
+			ArrayList<Knights_square> temp_squares = new ArrayList<>();
+			
+			for (int j = 0; j < side_length; j += 1) {
+				
+				Knights_square square = new Knights_square(curr_x, curr_y, side_length, curr_square);
+				temp_squares.add(square);
+				
+				//Increment curr_square 
+				curr_square += 1;
+				
+				//Increment curr_x
+				curr_x += side_length;
+			}
+			
+			squares.add(temp_squares);
+			
+			//Add side length to the curr_y
+			curr_y += side_length;
+			
+			//Reset curr_x
+			curr_x = 0;
+		}
+		
+		//Get the starting point
+		int start = 0;
+		while (true) {
+			System.out.print("What square do you want to start with? > ");
+			String start_point = input.nextLine();
+			
+			
+			try {
+				start = Integer.parseInt(start_point);
+				if (start <= total_boxes && start > 0) {
+					//start -= 1;
+					break;
+				} else {
+					continue;
+				}
+				
+			} catch (NumberFormatException e) {
+				System.out.println();
+				continue;
+			}
+		}
+		
+		//graphics.fill_a_square(start);
+		
+		start_knights_tour(start, squares, graphics);
+
+	}
+	
+	static void start_knights_tour(int start_point, ArrayList<ArrayList<Knights_square>> square_array, Knights_graphics graphics) {
+		int curr_big = 0;
+		int curr_little = 0;
+
+		//Find indexes of the starting point
+		for (int i = 0; i < square_array.size(); i += 1) {
+			for (int j = 0; j < square_array.get(i).size(); j += 1) {
+				if (square_array.get(i).get(j).get_id() == start_point) {
+					curr_big = i;
+					curr_little = j;
+				}
+			}
+		}
+		
+		if (solve_knights_tour(curr_big, curr_little, square_array, graphics)) {
+			graphics.solve_it();
+		} else {
+			graphics.fail();
+		}
+		
+		System.out.println("Done");
+		
+		
+	}
+	
+	//Big means outer array, small means inner array
+	public static boolean solve_knights_tour(int curr_big, int curr_little, ArrayList<ArrayList<Knights_square>> square_array, Knights_graphics graphics) {
+		
+		try {
+			Thread.sleep(75);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		boolean solved = false;
+		int little_size = square_array.get(0).size();
+		int big_size = square_array.size();
+		
+		int new_big = 0;
+		int new_little = 0;
+		
+		int fill_id = square_array.get(curr_big).get(curr_little).get_id();
+		graphics.fill_a_square(fill_id);
+		square_array.get(curr_big).get(curr_little).fill();
+		
+		
+		
+		//First square to try
+		new_big = curr_big - 2;
+		new_little = curr_little - 1;
+		if (new_big >= 0 && new_little >= 0 && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Second square
+		new_big = curr_big - 2;
+		new_little = curr_little + 1;
+		if (new_big >= 0 && new_little < little_size && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Third square
+		new_big = curr_big - 1;
+		new_little = curr_little + 2;
+		if (new_big >= 0 && new_little < little_size && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+			
+		}
+		
+		//Fourth square
+		new_big = curr_big + 1;
+		new_little = curr_little + 2;
+		if (new_big < big_size && new_little < little_size && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Fifth square
+		new_big = curr_big + 2;
+		new_little = curr_little + 1;
+		if (new_big < big_size && new_little < little_size && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Sixth square
+		new_big = curr_big + 2;
+		new_little = curr_little - 1;
+		if (new_big < big_size && new_little >= 0 && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Seventh square
+		new_big = curr_big + 1;
+		new_little = curr_little - 2;
+		if (new_big < big_size && new_little >= 0 && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Eighth square
+		new_big = curr_big - 1;
+		new_little = curr_little - 2;
+		if (new_big >= 0 && new_little >= 0 && square_array.get(new_big).get(new_little).is_open()) {
+			
+			if (solve_knights_tour(new_big, new_little, square_array, graphics)) {
+				return true;
+			}
+			
+		}
+		
+		//Check to see if I've 'won'
+		if (is_solved(square_array)) {
+			return true;
+		}
+		
+		graphics.unfill_a_square(fill_id);
+		square_array.get(curr_big).get(curr_little).unfill();
+		
+		return false;
+	}
+	
+	static boolean is_solved(ArrayList<ArrayList<Knights_square>> squares) {
+		boolean solved = true;
+		
+		for (int i = 0; i < squares.size(); i += 1) {
+			for (int j = 0; j < squares.get(0).size(); j += 1) {
+				if (squares.get(i).get(j).is_open()) {
+					solved = false;
+				}
+				
+			}
+		}
+		
+		return solved;
+	}
+
+}
